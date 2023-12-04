e5935717e7c1db3d96a0a6cf1546c47c3c683555
Cauê Melo <cauesmelo@gmail.com>
2023-12-04 20:05:10 -0300

    add day 4 solution part 2

--- solutions/day4.go
+++ solutions/day4.go
@@ -9,6 +9,12 @@ import (
 	"github.com/cauesmelo/aoc-2023/util"
 )
 
+type card struct {
+	position int
+	matchs   int
+	copies   int
+}
+
 func getNumbers(line string) []int {
 	re := regexp.MustCompile(`\d+`)
 
@@ -66,8 +72,67 @@ func Day4_part1() int {
 	return sum
 }
 
-func Day4_part2() int {
-	_ = util.GetInput(4, false)
+func procLinev2(line string, index int) card {
+	lineSplit := strings.Split(line, ": ")
+	numbers := strings.Split(lineSplit[1], " | ")
+	lineNumber := index + 1
 
-	return 0
+	winStr := numbers[0]
+	haveStr := numbers[1]
+
+	win := getNumbers(winStr)
+	have := getNumbers(haveStr)
+
+	match := make([]int, 0)
+
+	for _, n := range win {
+		for _, j := range have {
+			if n == j {
+				match = append(match, j)
+			}
+		}
+	}
+
+	return card{
+		position: lineNumber,
+		copies:   0,
+		matchs:   len(match),
+	}
+}
+
+func procCopy(cards []card, i int) []card {
+	multiplier := 1 + cards[i].copies
+	matchsCounter := cards[i].matchs
+	offSet := 1
+
+	for matchsCounter > 0 {
+		cards[i+offSet].copies = cards[i+offSet].copies + multiplier
+
+		matchsCounter--
+		offSet++
+	}
+
+	return cards
+}
+
+func Day4_part2() int {
+	lines := util.GetInput(4, false)
+
+	cards := make([]card, 0)
+
+	for i, line := range lines {
+		cards = append(cards, procLinev2(line, i))
+	}
+
+	for i := range cards {
+		cards = procCopy(cards, i)
+	}
+
+	total := 0
+
+	for _, card := range cards {
+		total = total + card.copies + 1
+	}
+
+	return total
 }

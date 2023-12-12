79f80a2be6a3f29fe16221a8ae85c5f22b57d31c
Cauê Melo <cauesmelo@gmail.com>
2023-12-11 22:49:59 -0300

    add day 8 part 2

--- solutions/day8.go
+++ solutions/day8.go
@@ -47,7 +47,7 @@ func moveUntilZZZ(moves string, m map[string]node) int {
 			curr = m[curr].left
 		}
 
-		if moves[mvIdx] == 'R' && m[curr].left != "" {
+		if moves[mvIdx] == 'R' && m[curr].right != "" {
 			curr = m[curr].right
 		}
 
@@ -58,18 +58,85 @@ func moveUntilZZZ(moves string, m map[string]node) int {
 	return count
 }
 
+func GCD(a, b int) int {
+	for b != 0 {
+		t := b
+		b = a % b
+		a = t
+	}
+	return a
+}
+
+func LCM(a, b int, integers []int) int {
+	result := a * b / GCD(a, b)
+
+	for i := 0; i < len(integers); i++ {
+		result = LCM(result, integers[i], integers[i+1:])
+	}
+
+	return result
+}
+
+func moveUntilZZZSim(moves string, m map[string]node) int {
+	count := 0
+
+	ghosts := make([]string, 0)
+	steps := make([]int, 0)
+
+	for k := range m {
+		if k[2] == 'A' {
+			ghosts = append(ghosts, k)
+		}
+	}
+
+	mvIdx := 0
+
+	for {
+		for i, g := range ghosts {
+			if g[2] == 'Z' {
+				ghosts = append(ghosts[:i], ghosts[i+1:]...)
+				steps = append(steps, count)
+			}
+		}
+
+		if len(ghosts) == 0 {
+			break
+		}
+
+		if mvIdx >= len(moves) {
+			mvIdx = 0
+		}
+
+		for i, curr := range ghosts {
+			if moves[mvIdx] == 'L' && m[curr].left != "" {
+				ghosts[i] = m[curr].left
+			}
+
+			if moves[mvIdx] == 'R' && m[curr].right != "" {
+				ghosts[i] = m[curr].right
+			}
+		}
+
+		mvIdx++
+		count++
+	}
+
+	return LCM(steps[0], steps[1], steps[2:])
+}
+
 func Day8_part1() int {
 	lines := util.GetInput(8, false)
-
 	tree := parseTree(lines[2:])
-
 	moves := moveUntilZZZ(lines[0], tree)
 
 	return moves
 }
 
 func Day8_part2() int {
-	_ = util.GetInput(8, false)
+	lines := util.GetInput(8, false)
 
-	return 0
+	tree := parseTree(lines[2:])
+	moves := moveUntilZZZSim(lines[0], tree)
+
+	return moves
 }
